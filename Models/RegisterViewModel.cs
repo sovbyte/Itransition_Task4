ed7d54fdceefe1b28127c4498e63a7bc79eca67d
sovbyte <sovbyte@gmail.com>
2026-04-15 04:17:32 +0500

    added RegisterViewModel.cs

--- Models/RegisterViewModel.cs
+++ Models/RegisterViewModel.cs
@@ -1,6 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Itransition_Task4.Models;
 
 public class RegisterViewModel
 {
+    [Required(ErrorMessage = "Enter Name")]
+    public string Name { get; set; } = string.Empty;
     
+    [Required(ErrorMessage = "Enter Email")]
+    [EmailAddress(ErrorMessage = "Incorrect Email format")]
+    public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Enter password")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Enter Confirm password")]
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; } = string.Empty;
 }
